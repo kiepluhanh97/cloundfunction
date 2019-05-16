19cfc18ef765f7cb6e77e135d128516a2cb3b050
Trần Quốc Pháp <1512395@hcmut.edu.vn>
2019-05-16 09:48:23 +0700

    test push

---
+++ logger/logger.go
@@ -0,0 +1,56 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var (
+	Log *Logger
+)
+
+type Logger struct {
+	*log.Logger
+}
+
+func NewLogger(fileName string, maxSize int, maxBackups int, maxAge int, compress bool) (*Logger, error) {
+	logDir := filepath.Dir(fileName)
+	log.Println("logDir", fileName, logDir)
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		err = os.MkdirAll(logDir, 0777)
+	}
+	var out io.Writer = &lumberjack.Logger{
+		Filename:   fileName,
+		MaxSize:    maxSize,
+		MaxBackups: maxBackups,
+		MaxAge:     maxAge,
+		Compress:   compress,
+		LocalTime:  true,
+	}
+
+	flag := log.LstdFlags | log.Lshortfile | log.Lmicroseconds
+	logger := log.New(io.MultiWriter(out, os.Stdout), "", flag)
+
+	return &Logger{logger}, nil
+}
+
+func (log *Logger) write(msg string) {
+	log.Print(msg)
+}
+
+func (log *Logger) Info(v ...interface{}) {
+	log.write("[INFO] " + ": " + fmt.Sprintln(v...))
+}
+
+func (log *Logger) Error(v ...interface{}) {
+	log.write("[ERROR] " + ": " + fmt.Sprintln(v...))
+}
+
+func (log *Logger) Warn(v ...interface{}) {
+	log.write("[WARN] " + ": " + fmt.Sprintln(v...))
+}
